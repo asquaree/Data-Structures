fc1d1acb3fc411a2b9d3baebc49bea4d303343be
aakash aggarwal <aakashaggarwal2311@gmail.com>
2020-12-29 12:25:26 +0530

    Update #2_print(n->1).cpp

--- Recursion/Aditya verma examples/#2_print(n->1).cpp
+++ Recursion/Aditya verma examples/#2_print(n->1).cpp
@@ -9,12 +9,8 @@ int main()
 {
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
-
-    int n;
-    cin>>n;
-
-    print_num(n);
-
+    
+    print_num(7);
 }
 /////////////OUTPUT/////////////////////////////////////////////////////
 //7 6 5 4 3 2 1 
